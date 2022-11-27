5740ce51f4ee81fdb01e4e43a216dc259df1be23
Justin Dionisio <justindionisio21@yahoo.com>
2022-11-26 19:24:21 -0800

    Adding clickevent and DOM manipulation

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+// document.querySelector('.message').textContent = '🎊 Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+
+document.querySelector('.check').addEventListener('click', function() {
+	const guess = Number(document.querySelector('.guess').value);
+	console.log(guess, typeof guess);
+
+	if (!guess) {
+		document.querySelector('.message').textContent = '⛔️ No number!';
+	}
+});
