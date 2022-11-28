e1bba6c4ada24f05e83566d5f1362d93f1000077
Justin Dionisio <justindionisio21@yahoo.com>
2022-11-28 14:29:34 -0800

    Manipulated CSS

--- starter/script.js
+++ starter/script.js
@@ -16,11 +16,19 @@ document.querySelector('.check').addEventListener('click', function() {
 	const guess = Number(document.querySelector('.guess').value);
 	console.log(guess, typeof guess);
 
-	// if else for no guess, guess is too high or too low and changes score with every wrong guess
+	// when there is no guess input
 	if (!guess) {
 		document.querySelector('.message').textContent = '⛔️ No number!';
+
+		// When player wins
 	} else if (guess === secretNumber) {
 		document.querySelector('.message').textContent = '🎊 Correct Number!';
+
+		document.querySelector('body').style.backgroundColor = '#60b347';
+
+		document.querySelector('.number').style.width = '30rem';
+
+		// When guess is too high
 	} else if (guess > secretNumber) {
 		if (score > 1) {
 			document.querySelector('.message').textContent = '📈 Too high!';
@@ -30,6 +38,8 @@ document.querySelector('.check').addEventListener('click', function() {
 			document.querySelector('.message').textContent = 'You lost the game! 💥';
 			document.querySelector('.score').textContent = 0;
 		}
+
+		// when guess is too low
 	} else if (guess < secretNumber) {
 		if (score > 1) {
 			document.querySelector('.message').textContent = '📉 Too low!';
